a266f403da0ae8ed5ac6f588e94fc02fabbdc92a
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-08 14:58:19 +0200

    7

---
+++ easypark/lib/homepage.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Hello,',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Jerome Bell',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Icon(Icons.person),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.pink[100],
+                    borderRadius: BorderRadius.circular(12)),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/doc.jpg'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+
+                      // color: Colors.deepPurple[600],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Text(
+                          'How do you feel? ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Detect wether you have Parkinson disease',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[300],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Center(
+                            child: Text('Get Started'),
+                          ),
+                        )
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: (Icon(Icons.search)),
+                      border: InputBorder.none,
+                      hintText: 'How can we help you?'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              height: 80,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.deepPurple[100],
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/detect.png', height: 30),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Detect Parkinson'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.deepPurple[100],
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/severity.png', height: 30),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Severity level'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.deepPurple[100],
+                      ),
+                      child: Row(
+                        //  Navigator.pushNamed(context, ''),
+                        children: [
+                          Image.asset('assets/questionnaire.jpg', height: 30),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Questionnaire'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.deepPurple[100],
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/doctor.png', height: 30),
+                          // ignore: prefer_const_constructors
+                          SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Contact Doctors'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  // ignore: prefer_const_constructors
+                  Text('Detection List',
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
+                  Text(
+                    'See all',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[500],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            Expanded(
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/doctor.png',
+                          height: 40,
+                        ),
+                        Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Icon(Icons.star),
+                            const Text('4'),
+                          ],
+                        ),
+                        const Text('Dr.Philip George'),
+                        const Text('6th of october , hospital '),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/doctor.png',
+                          height: 40,
+                        ),
+                        Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Icon(Icons.star),
+                            const Text('4'),
+                          ],
+                        ),
+                        const Text('Dr.Ahmed Marzoaa'),
+                        const Text('Alshrouq city , Swiss hospital '),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
