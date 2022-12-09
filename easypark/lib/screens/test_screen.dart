37bf11ef28804a54f94aac87dcd95a383604df92
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-12-10 00:29:27 +0200

    creating test page to choose from the type of test

---
+++ easypark/lib/screens/test_screen.dart
@@ -0,0 +1,255 @@
+import 'package:easypark/widgets/emotion_face.dart';
+import 'package:easypark/widgets/test_options.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  State<TestPage> createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.assistant),
+          label: '',
+        )
+      ]),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  // greetings row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: const Text(
+                              'Hi Jared',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('23 Kan, 2022',
+                              style: TextStyle(
+                                color: Colors.blue[200],
+                              ))
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius: BorderRadius.circular(12)),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  //Search Row
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue[600],
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  // how do you feel?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel today?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+
+                  // 4 different faces
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          EmotionFace(emtionFace: '🥲'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Bad',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmotionFace(emtionFace: ' 😅'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Fine',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmotionFace(emtionFace: '😃'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Well',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmotionFace(emtionFace: '😁'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Excellent',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: Container(
+                  padding: EdgeInsets.all(25),
+                  color: Colors.grey[200],
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Tests",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Icon(Icons.more_horiz)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        // list view of excercices
+                        Expanded(
+                          child: ListView(
+                            children: [
+                              TestOptions(
+                                icon: Icons.favorite,
+                                option: 'Hand-drawn Test',
+                                numberofoptions: 2,
+                                Color: Colors.blue,
+                              ),
+                              TestOptions(
+                                icon: Icons.favorite,
+                                option: 'Voice speech Test',
+                                numberofoptions: 2,
+                                Color: Colors.pink,
+                              ),
+                              TestOptions(
+                                icon: Icons.favorite,
+                                option: 'Face Picture Test',
+                                numberofoptions: 2,
+                                Color: Colors.green,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
