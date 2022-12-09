998aca74eaea085ea0af75ee60f8463978c0a302
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-10 00:05:17 +0200

    main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'speechDetection.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomePage(),
+          'speech': (context) => speechPage(),
+        });
   }
 }
