2d0966875bc5fd6b9a4928ae14a27087856c4e67
sajasaadoun <saja1904826@miuegypt.edu.eg>
2022-12-16 16:31:04 +0200

    home

--- easypark/lib/screens/homepage.dart
+++ easypark/lib/screens/homepage.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: SafeArea(
