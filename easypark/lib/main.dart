9b3abe040a3599879e1d8578b0f6e5ed08b23213
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2022-12-19 18:20:16 +0200

    update main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:easypark/screens/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/homepage.dart';
 import 'screens/speechDetection.dart';
+import 'screens/faceDetection.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,17 +13,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: '/register',
-        //home: Welcome(),
         routes: {
           '/': (context) => const HomePage(),
           '/test': (context) => const TestPage(),
-          '/speech': (context) => const speechPage(),
+          'speech': (context) => const speechPage(),
+          'face': (context) => const facePage(),
           '/login': (context) => const LoginScreen(),
           '/register': (context) => const RegisterScreen(),
         });
