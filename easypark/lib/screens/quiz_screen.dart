5e72c7406fc20c1117b7a37b93909dc984ee585c
Remon74 <remon1908077@miuegypt.edu.eg>
2022-12-20 09:27:51 +0200

    Update quiz_screen.dart

--- easypark/lib/screens/quiz_screen.dart
+++ easypark/lib/screens/quiz_screen.dart
@@ -18,7 +18,7 @@ class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 5, 50, 80),
+      backgroundColor: Colors.blue[600],
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
         child: Column(children: [
