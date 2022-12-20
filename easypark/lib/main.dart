39c4941ecf58203ca68d2f30e061df7c1061e840
Remon74 <remon1908077@miuegypt.edu.eg>
2022-12-20 09:26:42 +0200

    Update main.dart

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'screens/homepage.dart';
 import 'screens/speechDetection.dart';
 import 'screens/faceDetection.dart';
-import 'screens/question_page.dart';
+import 'screens/question_model.dart';
 import 'screens/doctor_page.dart';
 import 'screens/profile_page.dart';
 import 'screens/sketch_page.dart';
