011d65449cb6097c8c21f6f4f6fc4ef3c9b77fc1
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-18 15:05:21 +0200

    create login provider

---
+++ easypark/lib/provider/login_provider.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// // import '../../model/loginData.dart';
+// import '../dataApp/user_data.dart';
+
+// //---------------------UserProvider------------------------//
+
+// //provider byklm almodel
+// Stream userData = UserData().getUserDetails();
+// final userDataProviderRepository = StateProvider<Stream>((ref) => userData);
+// //gets the data from the model
+
+// final userDataProvider = StreamProvider(
+//     //bagyb beh aldata,byt3amel my al-ui
+//     //ui byklem provider
+//     (ref) => ref.watch(userDataProviderRepository));
+//     //listens to the data
+  
+
