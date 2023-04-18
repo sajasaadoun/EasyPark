967fb391122f92e7b234140f326b315b7d7f4054
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-18 15:06:05 +0200

    create new home page

---
+++ easypark/lib/screens/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import '../widgets/nearby_doctors.dart';
+import '../widgets/test_types.dart';
+import '../widgets/upcoming_card.dart';
+
+class HomePagee extends StatelessWidget {
+  const HomePagee({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Hi, Jane"),
+            Text(
+              "How are you feeling today?",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Ionicons.notifications_outline),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Ionicons.search_outline),
+          ),
+        ],
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(14),
+        children: [
+          const UpcomingCard(),
+          const SizedBox(height: 20),
+          Text(
+            "Health Needs",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(height: 15),
+          const HealthNeeds(),
+          const SizedBox(height: 25),
+          Text(
+            "Nearby Doctors",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(height: 15),
+          const NearbyDoctors(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.home_outline),
+            activeIcon: Icon(Ionicons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.calendar_outline),
+            activeIcon: Icon(Ionicons.calendar),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.chatbubble_ellipses_outline),
+            label: "Home",
+            activeIcon: Icon(Ionicons.chatbubble_ellipses),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.person_outline),
+            activeIcon: Icon(Ionicons.person),
+            label: "Home",
+          ),
+        ],
+      ),
+    );
+  }
+}
