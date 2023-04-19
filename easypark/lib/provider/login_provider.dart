8f2ebd12c9d771bfc909c661563786a4a7744294
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-19 03:29:08 +0200

    update login provider

--- easypark/lib/provider/login_provider.dart
+++ easypark/lib/provider/login_provider.dart
@@ -1,18 +1,19 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// // import '../../model/loginData.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../model/user_model.dart';
+// import '../../model/loginData.dart';
 // import '../dataApp/user_data.dart';
 
-// //---------------------UserProvider------------------------//
+//---------------------UserProvider------------------------//
 
-// //provider byklm almodel
-// Stream userData = UserData().getUserDetails();
-// final userDataProviderRepository = StateProvider<Stream>((ref) => userData);
-// //gets the data from the model
+//provider byklm almodel
+Stream userData = UserData().getUserDetails();
+final userDataProviderRepository = StateProvider<Stream>((ref) => userData);
+//gets the data from the model
 
-// final userDataProvider = StreamProvider(
-//     //bagyb beh aldata,byt3amel my al-ui
-//     //ui byklem provider
-//     (ref) => ref.watch(userDataProviderRepository));
-//     //listens to the data
+final userDataProvider = StreamProvider(
+    //bagyb beh aldata,byt3amel my al-ui
+    //ui byklem provider
+    (ref) => ref.watch(userDataProviderRepository));
+    //listens to the data
   
 
