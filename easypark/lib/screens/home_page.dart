55c2a804af87731e7c8a0714d1b2c19bc4f317f1
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-19 03:29:36 +0200

    update the home page

--- easypark/lib/screens/home_page.dart
+++ easypark/lib/screens/home_page.dart
@@ -1,24 +1,45 @@
+import 'package:easypark/screens/quiz_screen.dart';
+import 'package:easypark/screens/search.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import '../widgets/nearby_doctors.dart';
 import '../widgets/test_types.dart';
 import '../widgets/upcoming_card.dart';
 
-class HomePagee extends StatelessWidget {
+class HomePagee extends StatefulWidget {
   const HomePagee({Key? key}) : super(key: key);
 
   @override
+  _HomePageeState createState() => _HomePageeState();
+}
+
+class _HomePageeState extends State<HomePagee> {
+  int _selectedIndex = 0;
+
+  late BuildContext _context; // declare a variable to store context
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // _context = context; // store context in the variable
+
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
         automaticallyImplyLeading: false,
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Hi, Jane"),
+          children: const [
+            Text("Hi, Jane"),
             Text(
               "How are you feeling today?",
-              style: Theme.of(context).textTheme.caption,
+              style: TextStyle(
+                fontSize: 15,
+              ),
             ),
           ],
         ),
@@ -28,7 +49,12 @@ class HomePagee extends StatelessWidget {
             icon: const Icon(Ionicons.notifications_outline),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: SearchPage(),
+              );
+            },
             icon: const Icon(Ionicons.search_outline),
           ),
         ],
@@ -40,7 +66,7 @@ class HomePagee extends StatelessWidget {
           const UpcomingCard(),
           const SizedBox(height: 20),
           Text(
-            "Health Needs",
+            "Tests",
             style: Theme.of(context).textTheme.headline6,
           ),
           const SizedBox(height: 15),
@@ -58,6 +84,8 @@ class HomePagee extends StatelessWidget {
         type: BottomNavigationBarType.fixed,
         showUnselectedLabels: false,
         showSelectedLabels: false,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Ionicons.home_outline),
@@ -67,20 +95,47 @@ class HomePagee extends StatelessWidget {
           BottomNavigationBarItem(
             icon: Icon(Ionicons.calendar_outline),
             activeIcon: Icon(Ionicons.calendar),
-            label: "Home",
+            label: "Calendar",
           ),
           BottomNavigationBarItem(
             icon: Icon(Ionicons.chatbubble_ellipses_outline),
-            label: "Home",
+            label: "Chat",
             activeIcon: Icon(Ionicons.chatbubble_ellipses),
           ),
           BottomNavigationBarItem(
             icon: Icon(Ionicons.person_outline),
             activeIcon: Icon(Ionicons.person),
-            label: "Home",
+            label: "Profile",
           ),
         ],
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (_selectedIndex == 1) {
+        Navigator.push(
+          _context,
+          MaterialPageRoute(
+            builder: (context) => const HomePagee(),
+          ),
+        );
+      } else if (_selectedIndex == 2) {
+        Navigator.push(
+          _context,
+          MaterialPageRoute(
+            builder: (context) => const QuizScreen(),
+          ),
+        );
+      } else if (_selectedIndex == 3) {
+        Navigator.push(
+            _context,
+            MaterialPageRoute(
+              builder: (context) => const QuizScreen(),
+            ));
+      }
+    });
   }
 }
