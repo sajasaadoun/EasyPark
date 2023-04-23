b7a532c12bc1d5773f5bf362d92b7df0fa3c4858
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-04-23 16:55:31 +0200

    main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/home',
+        initialRoute: 'doctor',
         routes: {
           '/': (context) => const HomePage(),
           '/home': (context) => const HomePagee(),
