8b24ab7074b500f1a2342c13ef4351b2e1be2038
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-25 17:01:50 +0200

    update the sketch page

--- easypark/lib/screens/sketch_page.dart
+++ easypark/lib/screens/sketch_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:scribble/scribble.dart';
@@ -19,39 +21,45 @@ class _sketchPageState extends State<sketchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Draw a spiral or a normal wave'),
-        backgroundColor: Colors.deepPurple[100],
-        leading: IconButton(
-          icon: const Icon(Icons.save),
-          tooltip: "Save to Image",
-          onPressed: () => _saveImage(context),
+    return WillPopScope(
+      onWillPop: () async {
+        bool shouldExit = await _showExitConfirmationDialog(context);
+        return shouldExit;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Draw a spiral or a normal wave'),
+          backgroundColor: Colors.deepPurple[100],
+          leading: IconButton(
+            icon: const Icon(Icons.save),
+            tooltip: "Save to Image",
+            onPressed: () => _saveImage(context),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 2,
-          child: Stack(
-            children: [
-              Scribble(
-                notifier: notifier,
-                drawPen: true,
-              ),
-              Positioned(
-                top: 16,
-                right: 16,
-                child: Column(
-                  children: [
-                    _buildColorToolbar(context),
-                    const Divider(
-                      height: 32,
-                    ),
-                    _buildStrokeToolbar(context),
-                  ],
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 2,
+            child: Stack(
+              children: [
+                Scribble(
+                  notifier: notifier,
+                  drawPen: true,
                 ),
-              )
-            ],
+                Positioned(
+                  top: 16,
+                  right: 16,
+                  child: Column(
+                    children: [
+                      _buildColorToolbar(context),
+                      const Divider(
+                        height: 32,
+                      ),
+                      _buildStrokeToolbar(context),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -257,5 +265,37 @@ class _sketchPageState extends State<sketchPage> {
       backgroundColor: Colors.blueGrey,
       child: const Icon(Icons.clear),
     );
+  }
+
+  Future<bool> _showExitConfirmationDialog(BuildContext context) async {
+    return await showDialog<bool>(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('Instructions'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/spiralApp1.jpg',
+                        height: 150,
+                        width: 150,
+                      ),
+                      SizedBox(height: 16),
+                      Text('Step 1: Draw the Spiral as given. '),
+                      Text('Step 2: Press Submit.'),
+                      Text('Step 3: Finally, Check for the results.'),
+                    ],
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('OK'),
+                    ),
+                  ],
+                )) ??
+        false;
   }
 }
