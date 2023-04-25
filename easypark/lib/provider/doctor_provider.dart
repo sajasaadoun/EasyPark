abc77c4eafc8753d2c0ab9ff8efd2aa3a79f787b
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-04-25 17:00:45 +0200

    create a new doctor provider

---
+++ easypark/lib/provider/doctor_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../model/doctor_model.dart';
+
+//---------------------EventsProvider------------------------//
+
+//provider byklm almodel
+Stream doctorsData = DoctorsModel().doctorsDetails();
+final doctorsDataProviderRepository =
+    StateProvider<Stream>((ref) => doctorsData);
+//gets the data from the model
+
+final doctorsDataProvider = StreamProvider((
+        //bagyb beh aldata,byt3amel my al-ui
+        //ui byklem provider
+        (ref) => ref.watch(doctorsDataProviderRepository))
+    //listens to the data
+    );
+
+//////get with a condition ////////////////
+// Stream getSpecificdoc = DoctorsModel().getSpecificDoctor();
+// final SpecificdoctorProviderRepository =
+//     StateProvider<Stream>((ref) => getSpecificdoc);
+// //gets the data from the model
+
+// final SpecificdoctorProvider = StreamProvider((
+//         //bagyb beh aldata,byt3amel my al-ui
+//         //ui byklem provider
+//         (ref) => ref.watch(SpecificdoctorProviderRepository))
+//     //listens to the data
+//     );
