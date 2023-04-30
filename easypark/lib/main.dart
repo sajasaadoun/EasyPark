d9b87e5f915307519d5a18a158296ae890f641b1
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-04-30 21:14:51 +0300

    main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -1,28 +1,26 @@
-import 'package:easypark/screens/admin_screen.dart';
+import 'package:easypark/screens/OLDadmin_screen.dart';
 import 'package:easypark/screens/camera_screen.dart';
 import 'package:easypark/screens/doctor_info_screen.dart';
 import 'package:easypark/screens/handwrittingDetection.dart';
 import 'package:easypark/screens/home_page.dart';
+import 'package:easypark/screens/doctorpage.dart';
 import 'package:easypark/screens/login_screen.dart';
 import 'package:easypark/screens/options_screen.dart';
-import 'package:easypark/screens/register_page.dart';
 import 'package:easypark/screens/test_screen.dart';
+import 'package:easypark/screens/try.dart';
+import 'package:easypark/screens/admin_screen.dart';
 import 'package:easypark/screens/wave-detection.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'model/data_doctor.dart';
 import 'screens/dialog_message.dart';
-import 'screens/homepage.dart';
 // import 'screens/results_schedule_screen.dart';
 import 'screens/speechDetection.dart';
 import 'screens/faceDetection.dart';
 import 'screens/question_model.dart';
-import 'screens/doctor_page.dart';
+import 'screens/OLDdoctor_page.dart';
 import 'screens/profile_page.dart';
 import 'screens/sketch_page.dart';
-import 'screens/splash_screen.dart';
-import 'widgets/login_content.dart';
-import 'package:firebase_picture_uploader/firebase_picture_uploader.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
@@ -42,28 +40,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/home',
+        initialRoute: 'doctor',
         routes: {
-          // '/': (context) => const HomePage(),
-          '/home': (context) => const HomePagee(),
-          'test': (context) => const TestPage(),
+          'try': (context) =>  PatientReport(),
+          'admin': (context) =>  AdminPanell(),
+         'doctor': (context) => const DoctorPage(),
+          '/': (context) => const HomePagee(),
           'speech': (context) => const SpeechPage(),
-          'face': (context) => const FacePage(),
           'login': (context) => const LoginScreen(),
           'question': (context) => const QuestionsScreen(),
-          'doctor': (context) => const DoctorPage(),
           'profile': (context) => ProfilePage(),
           'sketch': (context) => const sketchPage(),
-          'admin': (context) => const MyAdmin(),
           'facepg': (context) => const FacePage(),
           'spiral': (context) => const HandwrittingDetection(),
           'waveDetection': (context) => const WaveDetection(),
-          'loginn': (context) => const LoginScreen(),
           'camera': (context) => const CameraScreen(),
           'DoctorsInfo': (context) => DoctorInfo(model: staticModel),
           'OptionsScreen': (context) => MyPlansScreen(),
           'MyAlertDialog': (context) => MyAlertDialog(),
           // 'splash': (context) => Splash()
+          // 'test': (context) => const TestPage(),
+          // 'admin': (context) => const MyAdmin(),
+      
+          // 'doctor': (context) => const DoctorPage(),
         });
   }
 }
