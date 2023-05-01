218b02c2328ce7c24fbf1d86a4d61814b99e1c70
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-05-01 04:41:14 +0300

    report

--- easypark/lib/screens/patientReport.dart
+++ easypark/lib/screens/patientReport.dart
@@ -7,17 +7,16 @@ class PatientReport extends StatefulWidget {
 
 class _PatientReportState extends State<PatientReport> {
   TextEditingController _medicationsController = TextEditingController();
-  TextEditingController _patientNameController = TextEditingController();
-  TextEditingController _ageController = TextEditingController();
-  TextEditingController _diagnosisController = TextEditingController();
 
-//  String _patientNameController = 'Ahmed Kamal';
-//   String _diagnosisController = 'Has parkinson disease';
+  String _patientName = 'Ahmed Kamal';
+  String _age = '65';
+  String _diagnosis = 'Has Parkinson disease';
+
   void _savePatientReport() {
     // Save the data entered by the user to a database or file.
-    print('Patient Name: ${_patientNameController.text}');
-    print('Diagnosis: ${_diagnosisController.text}');
-    print('Age: ${_ageController.text}');
+    print('Patient Name: $_patientName');
+    print('Diagnosis: $_diagnosis');
+    print('Age: $_age');
     print('Medications: ${_medicationsController.text}');
   }
 
@@ -59,11 +58,9 @@ class _PatientReportState extends State<PatientReport> {
                   ),
                 ),
                 SizedBox(height: 8.0),
-                TextField(
-                  controller: _patientNameController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter patient name',
-                  ),
+                Text(
+                  _patientName,
+                  style: TextStyle(fontSize: 16.0),
                 ),
                 SizedBox(height: 16.0),
                 Text(
@@ -73,11 +70,9 @@ class _PatientReportState extends State<PatientReport> {
                   ),
                 ),
                 SizedBox(height: 8.0),
-                TextField(
-                  controller: _ageController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter patient Age',
-                  ),
+                Text(
+                  _age,
+                  style: TextStyle(fontSize: 16.0),
                 ),
                 SizedBox(height: 16.0),
                 Text(
@@ -87,11 +82,9 @@ class _PatientReportState extends State<PatientReport> {
                   ),
                 ),
                 SizedBox(height: 8.0),
-                TextField(
-                  controller: _diagnosisController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter diagnosis',
-                  ),
+                Text(
+                  _diagnosis,
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ],
             ),
@@ -119,7 +112,7 @@ class _PatientReportState extends State<PatientReport> {
                 TextField(
                   controller: _medicationsController,
                   decoration: InputDecoration(
-                    hintText: 'Enter medications',
+                    hintText: 'Enter medications if needed',
                   ),
                 ),
               ],
@@ -135,73 +128,3 @@ class _PatientReportState extends State<PatientReport> {
     );
   }
 }
-
-// class PatientReport extends StatefulWidget {
-//   @override
-//   _PatientReportState createState() => _PatientReportState();
-// }
-
-// class _PatientReportState extends State<PatientReport> {
-//   TextEditingController _patientNameController = TextEditingController();
-//   TextEditingController _ageController = TextEditingController();
-//   TextEditingController _diagnosisController = TextEditingController();
-//   TextEditingController _medicationsController = TextEditingController();
-//   TextEditingController _remarksController = TextEditingController();
-
-//   void _savePatientReport() {
-//     // Save the data entered by the user to a database or file.
-//     print('Patient Name: ${_patientNameController.text}');
-//     print('Age: ${_ageController.text}');
-//     print('Diagnosis: ${_diagnosisController.text}');
-//     print('Medications: ${_medicationsController.text}');
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Patient Report'),
-//       ),
-//       body: SingleChildScrollView(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: <Widget>[
-//             TextField(
-//               controller: _patientNameController,
-//               decoration: InputDecoration(
-//                 labelText: 'Patient Name',
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextField(
-//               controller: _ageController,
-//               decoration: InputDecoration(
-//                 labelText: 'Age',
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextField(
-//               controller: _diagnosisController,
-//               decoration: InputDecoration(
-//                 labelText: 'Diagnosis',
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextField(
-//               controller: _medicationsController,
-//               decoration: InputDecoration(
-//                 labelText: 'Medications',
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//             ElevatedButton(
-//               child: Text('Save'),
-//               onPressed: _savePatientReport,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
