74a67ea3a780e16799d469125e2ab5d50479a4ed
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-05-02 23:24:39 +0300

    record

---
+++ easypark/lib/widgets/sound_recorder_when_locked_design.dart
@@ -0,0 +1,123 @@
+// library social_media_recorder;
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:easypark/provider/sound_record_notifier.dart';
+// import 'package:easypark/widgets/show_counter.dart';
+
+// // ignore: must_be_immutable
+// class SoundRecorderWhenLockedDesign extends StatelessWidget {
+//   final SoundRecordNotifier soundRecordNotifier;
+//   final String? cancelText;
+//   final Function sendRequestFunction;
+//   final Widget? recordIconWhenLockedRecord;
+//   final TextStyle? cancelTextStyle;
+//   final TextStyle? counterTextStyle;
+//   final Color recordIconWhenLockBackGroundColor;
+//   final Color? counterBackGroundColor;
+//   final Color? cancelTextBackGroundColor;
+//   final Widget? sendButtonIcon;
+//   // ignore: sort_constructors_first
+//   const SoundRecorderWhenLockedDesign({
+//     Key? key,
+//     required this.sendButtonIcon,
+//     required this.soundRecordNotifier,
+//     required this.cancelText,
+//     required this.sendRequestFunction,
+//     required this.recordIconWhenLockedRecord,
+//     required this.cancelTextStyle,
+//     required this.counterTextStyle,
+//     required this.recordIconWhenLockBackGroundColor,
+//     required this.counterBackGroundColor,
+//     required this.cancelTextBackGroundColor,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: MediaQuery.of(context).size.width,
+//       decoration: BoxDecoration(
+//         color: cancelTextBackGroundColor ?? Colors.grey.shade100,
+//         borderRadius: const BorderRadius.only(
+//           bottomRight: Radius.circular(24),
+//           topRight: Radius.circular(24),
+//         ),
+//       ),
+//       child: InkWell(
+//         onTap: () {
+//           soundRecordNotifier.isShow = false;
+//           soundRecordNotifier.resetEdgePadding();
+//         },
+//         child: Row(
+//           children: [
+//             InkWell(
+//               onTap: () async {
+//                 soundRecordNotifier.isShow = false;
+//                 if (soundRecordNotifier.second > 1 ||
+//                     soundRecordNotifier.minute > 0) {
+//                   String path = soundRecordNotifier.mPath;
+//                   await Future.delayed(const Duration(milliseconds: 500));
+//                   sendRequestFunction(File.fromUri(Uri(path: path)));
+//                 }
+//                 soundRecordNotifier.resetEdgePadding();
+//               },
+//               child: Transform.scale(
+//                 scale: 1.2,
+//                 child: ClipRRect(
+//                   borderRadius: BorderRadius.circular(600),
+//                   child: AnimatedContainer(
+//                     duration: const Duration(milliseconds: 500),
+//                     curve: Curves.easeIn,
+//                     width: 50,
+//                     height: 50,
+//                     child: Container(
+//                       color: recordIconWhenLockBackGroundColor,
+//                       child: Padding(
+//                         padding: const EdgeInsets.all(4.0),
+//                         child: recordIconWhenLockedRecord ??
+//                             sendButtonIcon ??
+//                             Icon(
+//                               Icons.send,
+//                               textDirection: TextDirection.ltr,
+//                               size: 28,
+//                               color: (soundRecordNotifier.buttonPressed)
+//                                   ? Colors.grey.shade200
+//                                   : Colors.black,
+//                             ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Expanded(
+//               child: InkWell(
+//                   onTap: () {
+//                     soundRecordNotifier.isShow = false;
+//                     soundRecordNotifier.resetEdgePadding();
+//                   },
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Text(
+//                       cancelText ?? "",
+//                       maxLines: 1,
+//                       textAlign: TextAlign.center,
+//                       overflow: TextOverflow.clip,
+//                       style: cancelTextStyle ??
+//                           const TextStyle(
+//                             color: Colors.black,
+//                           ),
+//                     ),
+//                   )),
+//             ),
+//             ShowCounter(
+//               soundRecorderState: soundRecordNotifier,
+//               counterTextStyle: counterTextStyle,
+//               counterBackGroundColor: counterBackGroundColor,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
