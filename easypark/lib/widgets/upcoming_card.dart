4ab8747728b5d58b0bf8af0291eef8ff431610c7
Remon74 <remon1908077@miuegypt.edu.eg>
2023-05-02 17:48:06 +0300

    Update upcoming_card.dart

--- easypark/lib/widgets/upcoming_card.dart
+++ easypark/lib/widgets/upcoming_card.dart
@@ -1,4 +1,4 @@
-import 'package:easypark/screens/homepage.dart';
+//import 'package:easypark/screens/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 
