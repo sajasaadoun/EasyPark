db0f505603d431acd679b912b5fda72520a61f85
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-05-06 20:59:08 +0300

    patientinfo provider

---
+++ easypark/lib/provider/patientInfo_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../model/patientInfoModel.dart';
+
+
+//---------------------EventsProvider------------------------//
+
+//provider byklm almodel
+Stream patientInfoData = patientInfoModel().patientInfoDetails();
+final patientInfoProviderRepository =
+    StateProvider<Stream>((ref) => patientInfoData);
+//gets the data from the model
+
+final patientInfoProvider = StreamProvider((
+        //bagyb beh aldata,byt3amel my al-ui
+        //ui byklem provider
+        (ref) => ref.watch(patientInfoProviderRepository))
+    //listens to the data
+    );
+
+//////get with a condition ////////////////
+// Stream getSpecificdoc = DoctorsModel().getSpecificDoctor();
+// final SpecificdoctorProviderRepository =
+//     StateProvider<Stream>((ref) => getSpecificdoc);
+// //gets the data from the model
+
+// final SpecificdoctorProvider = StreamProvider((
+//         //bagyb beh aldata,byt3amel my al-ui
+//         //ui byklem provider
+//         (ref) => ref.watch(SpecificdoctorProviderRepository))
+//     //listens to the data
+//     );
