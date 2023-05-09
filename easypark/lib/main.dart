ec407a3de0e7bce83e97c1373959788f3d2828e1
Remon74 <remon1908077@miuegypt.edu.eg>
2023-05-09 14:50:06 +0300

    main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -89,7 +89,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: 'edit',
+        initialRoute: 'question',
         routes: {
           'quest': (context) => AddQuestions(),
           'record': (context) => recordPage(),
