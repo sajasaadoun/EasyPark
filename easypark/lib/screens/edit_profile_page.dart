b8e5f5c8469f767c786f95f47a51f03bef31adef
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2023-05-10 19:20:38 +0300

    Update edit_profile_page.dart

--- easypark/lib/screens/edit_profile_page.dart
+++ easypark/lib/screens/edit_profile_page.dart
@@ -1,126 +1,197 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:easypark/model/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:easypark/utils/user_preferences.dart';
-import 'package:easypark/widgets/appbar_widget.dart';
+import 'package:easypark/provider/login_provider.dart';
+import '../model/user_model.dart';
 import 'package:easypark/widgets/profile_widget.dart';
 import 'package:easypark/widgets/textfield_widget.dart';
 
-class EditProfilePage extends StatefulWidget {
+class EditProfilePage extends ConsumerStatefulWidget {
   @override
-  _EditProfilePageState createState() => _EditProfilePageState();
+  ConsumerState<EditProfilePage> createState() => _EditProfilePageState();
 }
 
-class _EditProfilePageState extends State<EditProfilePage> {
-  User user = UserPreferences.myUser;
-
-  final _formKey = GlobalKey<FormState>();
+class _EditProfilePageState extends ConsumerState<EditProfilePage> {
+  var user = UserPreferences.myUser;
+  final userData = UserData();
+  // User user = UserPreferences.myUser;
+  PlatformFile? pickedFile;
+  ImageProvider? image;
+  late TextEditingController nameController;
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late TextEditingController ageController;
+  late TextEditingController phoneController;
 
   @override
+  void initState() {
+    super.initState(); // get old value and put it in txtfields
+    final watchValue = ref.read(userDataProvider).value;
+
+    nameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    ageController = TextEditingController();
+    phoneController = TextEditingController();
+  }
+
+//********************uploadfile ***********************/
+  Future uploadFile() async {
+    final path = 'files/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+    final ref = FirebaseStorage.instance.ref().child(path);
+    ref.putFile(file);
+  }
+
+//******************end-upload************** */
+
+//***************Select picture************** */
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    setState(() {
+      pickedFile = result.files.first;
+    });
+  }
+
+//*****************end-select******************* */
+  @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: buildAppBar(context),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 32),
-            physics: BouncingScrollPhysics(),
-            children: [
-              ProfileWidget(
-                imagePath: user.imagePath,
-                isEdit: true,
-                onClicked: () async {},
+        //appBar: buildAppBar(context),
+
+        body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          physics: BouncingScrollPhysics(),
+          children: [
+            const SizedBox(height: 50),
+            if (pickedFile != null)
+              Expanded(
+                  child: Container(
+                      color: Colors.blue[100],
+                      child: Image.file(
+                        File(pickedFile!.path!),
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ))),
+
+            ProfileWidget(
+              imagePath: user.imagePath,
+              isEdit: true,
+              onClicked: () async {},
+            ),
+            ElevatedButton(
+              onPressed: selectFile,
+              child: const Text("select image"),
+            ),
+            ElevatedButton(
+                onPressed: uploadFile, child: const Text("upload image")),
+            // ######################update################################
+            ElevatedButton(
+              child: Text("Update"),
+              onPressed: (() {
+                userData.updateUserDetails(
+                    nameController.text,
+                    emailController.text,
+                    passwordController.text,
+                    ageController.text,
+                    phoneController.text);
+              }),
+            ),
+            //#######################end-update#######################
+
+            //##############################delete########################
+            ElevatedButton(
+              child: const Text("Delete My account"),
+              onPressed: () {
+                final docUser =
+                    FirebaseFirestore.instance.collection('users').doc(userID);
+
+                docUser.update({
+                  'firstname': FieldValue.delete(),
+                  'lastname': FieldValue.delete(),
+                  'email': FieldValue.delete(),
+                  'nationality': FieldValue.delete(),
+                  'password': FieldValue.delete(),
+                  'DateOfBirth': FieldValue.delete(),
+                  'role': FieldValue.delete(),
+                  'phoneNum': FieldValue.delete(),
+                  'id': FieldValue.delete(),
+                });
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+            //#################################end-delete#############################
+            const SizedBox(height: 24),
+            TextField(
+              controller: nameController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name',
+                hintText: 'Enter your name',
               ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: user.name,
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-                onChanged: (value) {},
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Enter your email',
               ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: user.email,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  } else if (!value.contains('@') || !value.contains('.')) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-                onChanged: (value) {},
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: ageController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Age',
+                hintText: 'Enter your age',
               ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: '123456',
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  } else if (value.length < 8) {
-                    return 'Password must be at least 8 characters long';
-                  }
-                  return null;
-                },
-                onChanged: (value) {},
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: passwordController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter your password',
               ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: user.age.toString(),
-                decoration: InputDecoration(
-                  labelText: 'Age',
-                ),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your age';
-                  } else if (int.tryParse(value) == null) {
-                    return 'Please enter a valid age';
-                  }
-                  return null;
-                },
-                onChanged: (value) {},
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: phoneController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Phone',
+                hintText: 'Enter your phone number',
               ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: '01223456789',
-                decoration: InputDecoration(
-                  labelText: 'Phone Number',
-                ),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your phone number';
-                  } else if (int.tryParse(value) == null) {
-                    return 'Please enter a valid phone number';
-                  }
-                  return null;
-                },
-                onChanged: (value) {},
-              ),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Save the user's edited profile here
-                  }
-                },
-                child: Text('Edit'),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 24),
+          ],
         ),
       );
 }
+
+//  onPressed: ()async {
+//                 await FirebaseAuth.instance.signOut();
+
+//           // Get the current user's ID
+//           final user = FirebaseAuth.instance.currentUser;
+//           final userId = user?.uid;
+
+//           // Delete the user's data from Firebase
+//          await FirebaseFirestore.instance.collection('users').doc(userId).delete();
+
+//           // Navigate to the login screen
+//           Navigator.of(context).pushReplacementNamed('/h');
+//               }
