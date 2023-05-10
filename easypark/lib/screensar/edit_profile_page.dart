f01505542261ab3a671c2f062b407f879319ba85
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2023-05-10 22:15:36 +0300

    Update edit_profile_page.dart

--- easypark/lib/screensar/edit_profile_page.dart
+++ easypark/lib/screensar/edit_profile_page.dart
@@ -11,12 +11,12 @@ import '../model/user_model.dart';
 import 'package:easypark/widgets/profile_widget.dart';
 import 'package:easypark/widgets/textfield_widget.dart';
 
-class EditProfilePage extends ConsumerStatefulWidget {
+class EditProfilePageAr extends ConsumerStatefulWidget {
   @override
-  ConsumerState<EditProfilePage> createState() => _EditProfilePageState();
+  ConsumerState<EditProfilePageAr> createState() => _EditProfilePageState();
 }
 
-class _EditProfilePageState extends ConsumerState<EditProfilePage> {
+class _EditProfilePageState extends ConsumerState<EditProfilePageAr> {
   var user = UserPreferences.myUser;
   final userData = UserData();
   // User user = UserPreferences.myUser;
@@ -48,9 +48,9 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
     ref.putFile(file);
   }
 
-//******************end-upload************** */
+//*****************end-upload************* */
 
-//***************Select picture************** */
+//**************Select picture************* */
   Future selectFile() async {
     final result = await FilePicker.platform.pickFiles();
     if (result == null) return;
@@ -59,7 +59,7 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
     });
   }
 
-//*****************end-select******************* */
+//****************end-select****************** */
   @override
   Widget build(BuildContext context) => Scaffold(
         //appBar: buildAppBar(context),
@@ -84,48 +84,13 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
               isEdit: true,
               onClicked: () async {},
             ),
-            ElevatedButton(
-              onPressed: selectFile,
-              child: const Text("select image"),
-            ),
-            ElevatedButton(
-                onPressed: uploadFile, child: const Text("upload image")),
-            // ######################update################################
-            ElevatedButton(
-              child: Text("Update"),
-              onPressed: (() {
-                userData.updateUserDetails(
-                    nameController.text,
-                    emailController.text,
-                    passwordController.text,
-                    ageController.text,
-                    phoneController.text);
-              }),
-            ),
-            //#######################end-update#######################
+            // ElevatedButton(
+            //   onPressed: selectFile,
+            //   child: const Text("select image"),
+            // ),
+            // ElevatedButton(
+            //     onPressed: uploadFile, child: const Text("upload image")),
 
-            //##############################delete########################
-            ElevatedButton(
-              child: const Text("Delete My account"),
-              onPressed: () {
-                final docUser =
-                    FirebaseFirestore.instance.collection('users').doc(userID);
-
-                docUser.update({
-                  'firstname': FieldValue.delete(),
-                  'lastname': FieldValue.delete(),
-                  'email': FieldValue.delete(),
-                  'nationality': FieldValue.delete(),
-                  'password': FieldValue.delete(),
-                  'DateOfBirth': FieldValue.delete(),
-                  'role': FieldValue.delete(),
-                  'phoneNum': FieldValue.delete(),
-                  'id': FieldValue.delete(),
-                });
-                FirebaseAuth.instance.signOut();
-              },
-            ),
-            //#################################end-delete#############################
             const SizedBox(height: 24),
             TextField(
               controller: nameController,
@@ -177,21 +142,43 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
               ),
             ),
             const SizedBox(height: 24),
+            // ######################update################################
+            ElevatedButton(
+              child: Text("Update"),
+              onPressed: (() {
+                userData.updateUserDetails(
+                    nameController.text,
+                    emailController.text,
+                    passwordController.text,
+                    ageController.text,
+                    phoneController.text);
+              }),
+            ),
+            //#######################end-update#######################
+
+            //##############################delete########################
+            ElevatedButton(
+              child: const Text("Delete My account"),
+              onPressed: () {
+                final docUser =
+                    FirebaseFirestore.instance.collection('users').doc(userID);
+
+                docUser.update({
+                  'firstname': FieldValue.delete(),
+                  'lastname': FieldValue.delete(),
+                  'email': FieldValue.delete(),
+                  'nationality': FieldValue.delete(),
+                  'password': FieldValue.delete(),
+                  'DateOfBirth': FieldValue.delete(),
+                  'role': FieldValue.delete(),
+                  'phoneNum': FieldValue.delete(),
+                  'id': FieldValue.delete(),
+                });
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+            //#################################end-delete#############################
           ],
         ),
       );
 }
-
-//  onPressed: ()async {
-//                 await FirebaseAuth.instance.signOut();
-
-//           // Get the current user's ID
-//           final user = FirebaseAuth.instance.currentUser;
-//           final userId = user?.uid;
-
-//           // Delete the user's data from Firebase
-//          await FirebaseFirestore.instance.collection('users').doc(userId).delete();
-
-//           // Navigate to the login screen
-//           Navigator.of(context).pushReplacementNamed('/h');
-//               }
