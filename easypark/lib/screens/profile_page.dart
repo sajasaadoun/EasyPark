e3c2919c29896a63bc78b02f01391adde38b1f83
Remon74 <remon1908077@miuegypt.edu.eg>
2023-05-10 20:19:24 +0300

    Update profile_page.dart

--- easypark/lib/screens/profile_page.dart
+++ easypark/lib/screens/profile_page.dart
@@ -1,18 +1,4 @@
 
-//   Widget buildName(User user) => Column(
-//         children: [
-//           Text(
-//             user.name,
-//             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-//           ),
-//           const SizedBox(height: 4),
-//           Text(
-//             user.email,
-//             style: TextStyle(color: Colors.grey),
-//           )
-//         ],
-//       );
-
 //   Widget buildAbout(User user) => Container(
 //         padding: EdgeInsets.symmetric(horizontal: 48),
 //         child: Column(
