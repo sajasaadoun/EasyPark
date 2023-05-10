84871de181cecea8cc44256a75f58fc378b65091
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2023-05-10 23:10:38 +0300

    Update edit_profile_page.dart

--- easypark/lib/screens/edit_profile_page.dart
+++ easypark/lib/screens/edit_profile_page.dart
@@ -11,34 +11,23 @@ import '../model/user_model.dart';
 import 'package:easypark/widgets/profile_widget.dart';
 import 'package:easypark/widgets/textfield_widget.dart';
 
-class EditProfilePage extends ConsumerStatefulWidget {
+class EditProfilePage extends StatefulWidget {
   @override
-  ConsumerState<EditProfilePage> createState() => _EditProfilePageState();
+  State<EditProfilePage> createState() => _EditProfilePageState();
 }
 
-class _EditProfilePageState extends ConsumerState<EditProfilePage> {
+class _EditProfilePageState extends State<EditProfilePage> {
   var user = UserPreferences.myUser;
   final userData = UserData();
   // User user = UserPreferences.myUser;
   PlatformFile? pickedFile;
   ImageProvider? image;
-  late TextEditingController nameController;
-  late TextEditingController emailController;
-  late TextEditingController passwordController;
-  late TextEditingController ageController;
-  late TextEditingController phoneController;
-
-  @override
-  void initState() {
-    super.initState(); // get old value and put it in txtfields
-    final watchValue = ref.read(userDataProvider).value;
-
-    nameController = TextEditingController();
-    emailController = TextEditingController();
-    passwordController = TextEditingController();
-    ageController = TextEditingController();
-    phoneController = TextEditingController();
-  }
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  final formkey = GlobalKey<FormState>();
 
 //********************uploadfile ***********************/
   Future uploadFile() async {
@@ -61,124 +50,207 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
 
 //****************end-select****************** */
   @override
-  Widget build(BuildContext context) => Scaffold(
-        //appBar: buildAppBar(context),
-
-        body: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 32),
-          physics: BouncingScrollPhysics(),
-          children: [
-            const SizedBox(height: 50),
-            if (pickedFile != null)
-              Expanded(
-                  child: Container(
-                      color: Colors.blue[100],
-                      child: Image.file(
-                        File(pickedFile!.path!),
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ))),
-
-            ProfileWidget(
-              imagePath: user.imagePath,
-              isEdit: true,
-              onClicked: () async {},
-            ),
-            // ElevatedButton(
-            //   onPressed: selectFile,
-            //   child: const Text("select image"),
-            // ),
-            // ElevatedButton(
-            //     onPressed: uploadFile, child: const Text("upload image")),
-
-            const SizedBox(height: 24),
-            TextField(
-              controller: nameController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Name',
-                hintText: 'Enter your name',
-              ),
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-                hintText: 'Enter your email',
-              ),
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              controller: ageController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Age',
-                hintText: 'Enter your age',
-              ),
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              controller: passwordController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              controller: phoneController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Phone',
-                hintText: 'Enter your phone number',
-              ),
-            ),
-            const SizedBox(height: 24),
-            // ######################update################################
-            ElevatedButton(
-              child: Text("Update"),
-              onPressed: (() {
-                userData.updateUserDetails(
-                    nameController.text,
-                    emailController.text,
-                    passwordController.text,
-                    ageController.text,
-                    phoneController.text);
-              }),
-            ),
-            //#######################end-update#######################
-
-            //##############################delete########################
-            ElevatedButton(
-              child: const Text("Delete My account"),
-              onPressed: () {
-                final docUser =
-                    FirebaseFirestore.instance.collection('users').doc(userID);
-
-                docUser.update({
-                  'firstname': FieldValue.delete(),
-                  'lastname': FieldValue.delete(),
-                  'email': FieldValue.delete(),
-                  'nationality': FieldValue.delete(),
-                  'password': FieldValue.delete(),
-                  'DateOfBirth': FieldValue.delete(),
-                  'role': FieldValue.delete(),
-                  'phoneNum': FieldValue.delete(),
-                  'id': FieldValue.delete(),
-                });
-                FirebaseAuth.instance.signOut();
-              },
-            ),
-            //#################################end-delete#############################
-          ],
+  Widget build(BuildContext context) {
+    //appBar: buildAppBar(context),
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        title: const Text(
+          style: TextStyle(fontSize: 20, color: Colors.white),
+          'Edit your profile',
         ),
-      );
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/ar/home');
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Form(
+                key: formkey,
+                child: Column(
+                  children: [
+                    // padding: const EdgeInsets.symmetric(horizontal: 32),
+
+                    const SizedBox(height: 50),
+                    if (pickedFile != null)
+                      Expanded(
+                          child: Container(
+                              color: Colors.blue[100],
+                              child: Image.file(
+                                File(pickedFile!.path!),
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ))),
+
+                    ProfileWidget(
+                      imagePath: user.imagePath,
+                      isEdit: true,
+                      onClicked: () async {},
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 25, 10, 10),
+                      child: TextFormField(
+                          controller: nameController,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Name',
+                            hintText: 'Enter your name',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                !RegExp('[a-zA-Z]').hasMatch(value)) {
+                              return "enter a correct name";
+                            } else {
+                              return null;
+                            }
+                          }),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                          controller: emailController,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                            hintText: 'Enter your email',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                !RegExp('.+@.+\..+').hasMatch(value)) {
+                              return "enter a city";
+                            } else {
+                              return null;
+                            }
+                          }),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        controller: ageController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Age',
+                          hintText: 'Enter your age',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        controller: passwordController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                          controller: phoneController,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Phone',
+                            hintText: 'Enter your phone number',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your phone number';
+                            } else if (!RegExp(r'^\d{11}$').hasMatch(value)) {
+                              return 'Your phone number should be 11 number';
+                            } else {
+                              return null;
+                            }
+                          }),
+                    ),
+                    // ######################update################################
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: ElevatedButton(
+                        child: const Text("Update"),
+                        onPressed: (() async {
+                          if (formkey.currentState!.validate()) {
+                            try {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Updated Successfully')),
+                              );
+                              await userData.updateUserDetails(
+                                  nameController.text,
+                                  emailController.text,
+                                  passwordController.text,
+                                  ageController.text,
+                                  phoneController.text);
+                              Navigator.pushNamed(context, 'edit');
+                            } catch (error) {
+                              print(
+                                  'Error: $error'); // Print the error in the terminal
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text(
+                                      'There was an error, Please try again!'),
+                                ),
+                              );
+                            }
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Correct the mistakes in form.'),
+                              ),
+                            );
+                          }
+                          userData.updateUserDetails(
+                              nameController.text,
+                              emailController.text,
+                              passwordController.text,
+                              ageController.text,
+                              phoneController.text);
+                        }),
+                      ),
+                    ),
+                    //#######################end-update#######################
+
+                    //##############################delete########################
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: ElevatedButton(
+                        child: const Text("Delete My account"),
+                        onPressed: () {
+                          final docUser = FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(userID);
+
+                          docUser.delete();
+                          FirebaseAuth.instance.signOut();
+                          Navigator.pushNamed(context, 'login');
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
 }
