9d2b897412612488707eeb03d2d9037f347b70d1
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2023-05-10 19:20:34 +0300

    Update user_model.dart

--- easypark/lib/model/user_model.dart
+++ easypark/lib/model/user_model.dart
@@ -1,79 +1,52 @@
-import 'dart:math';
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:easypark/model/user_data.dart';
 
-final user = FirebaseAuth.instance.currentUser!;
-String userID = user.uid;
+import 'dart:convert';
+import 'dart:core';
 
-class UserData {
-  // Future<Object> getUserDetails() async {
-  //   final user = FirebaseAuth.instance.currentUser!;
-  //   String userIds = user.uid;
-  //   final DocumentSnapshot documentSnapshot =
-  //       await FirebaseFirestore.instance.collection('users').doc(userIds).get();
-  //   return documentSnapshot;
-  // }
+class UserModel {
+  String name;
+  String email;
+  String password;
+  String about;
 
-  Stream<DocumentSnapshot> getUserDetails() {
-    final user = FirebaseAuth.instance.currentUser!;
-    String userIds = user.uid;
-    return FirebaseFirestore.instance
-        .collection('users')
-        .doc(userIds)
-        .snapshots();
+  UserModel(
+      {required this.name,
+      required this.about,
+      required this.email,
+      required this.password});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'about': about,
+      'password': password,
+      'email': email,
+    };
   }
 
-  Future updateUserDetails(
-      String userName, String userEmail, String password, String ln) async {
-    final updateUser =
-        FirebaseFirestore.instance.collection('users').doc(userID);
-    updateUser.update(
-      {
-        'firstname': userName.trim(),
-        'email': userEmail.trim(),
-        'password': password.trim(),
-        'lastname': ln.trim(),
-      },
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] as String,
+      about: map['about'] as String,
+      password: map['password'] as String,
+      email: map['email'] as String,
     );
   }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
 
-// File? _image;
-// String downloadURL = '';
-
-// Future saveImage() async {
-//   Reference referenceRoot = FirebaseStorage.instance.ref();
-//   Reference referenceDirImage = referenceRoot.child('image');
-//   String fileName =
-//       '${DateTime.now().millisecondsSinceEpoch}-${Random().nextInt(100000)}.jpg';
-//   Reference referenceImageToUpload = referenceDirImage.child(fileName);
-//   try {
-//     await referenceImageToUpload.putFile(File(_image!.path));
-//     downloadURL = await referenceImageToUpload.getDownloadURL();
-//   } catch (e) {
-//     print(e);
-//   }
-//   setState(() {
-//     _image = File(_image!.path);
-//   });
-// }
-
-class User {
-  final String imagePath;
-  final String name;
-  final String email;
-  final String password;
-  final String about;
-  final bool isDarkMode;
-
-  const User({
-    required this.imagePath,
-    required this.name,
-    required this.email,
-    required this.password,
-    required this.about,
-    required this.isDarkMode,
-  });
+class UserPreferences {
+  static const myUser = User(
+    imagePath: 'https://cdn-icons-png.flaticon.com/512/146/146007.png',
+    name: 'EgyMania User',
+    email: 'EgyManiaUser@gmail.com',
+    password: '123456',
+    about:
+        'A MIU Computer science students works on EgyMania project acts as User in this application.',
+    isDarkMode: false,
+  );
 }
