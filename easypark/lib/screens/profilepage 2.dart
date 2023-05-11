e6afad23ed9cfc0b053c1f2c1df08aaed4aa19e9
Remon74 <remon1908077@miuegypt.edu.eg>
2023-05-11 16:04:29 +0300

    Update profilepage 2.dart

--- easypark/lib/screens/profilepage 2.dart
+++ easypark/lib/screens/profilepage 2.dart
@@ -1,23 +1,4 @@
-              //               width: 40,
-              //               height: 40,
-              //               fit: BoxFit.fill,
-              //             ),
-              //             SizedBox(width: 10),
-              //             Container(
-              //               height: 40,
-              //               width: 40,
-              //               color: Colors.orangeAccent[100],
-              //               child: Align(
-              //                   alignment: Alignment.center,
-              //                   child: Icon(
-              //                     Icons.add_photo_alternate_outlined,
-              //                     color: Colors.black,
-              //                   )),
-              //             )
-            ],
-          )),
-    );
-  }
+    
 
   // ignore: non_constant_identifier_names
   Widget Card(String a, String b, double x, double y) {
