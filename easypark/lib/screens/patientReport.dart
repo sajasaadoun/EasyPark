015cc3064f80b43a1956d4dc738a84e92de005f0
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-05-12 22:25:18 +0300

    patientrep

--- easypark/lib/screens/patientReport.dart
+++ easypark/lib/screens/patientReport.dart
@@ -14,7 +14,8 @@ class _PatientReportState extends ConsumerState<PatientReport> {
 
   String _patientName = 'Amgad Besheer';
   String _age = '56';
-  String _diagnosis = 'Has Parkinson disease';
+  String _diagnosis = 'Parkinson';
+  String _Ddiagnosis = 'Has Parkinson disease';
 
   void _savePatientReport() {
     // Save the data entered by the user to a database or file.
@@ -92,7 +93,7 @@ class _PatientReportState extends ConsumerState<PatientReport> {
                 ),
                 SizedBox(height: 8.0),
                 Text(
-                  _diagnosis,
+                  _Ddiagnosis,
                   style: TextStyle(fontSize: 16.0),
                 ),
                 SizedBox(height: 16.0),
@@ -197,169 +198,3 @@ class _PatientReportState extends ConsumerState<PatientReport> {
     );
   }
 }
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// class PatientReport extends StatefulWidget {
-//   final DocumentSnapshot document;
-
-//   PatientReport(this.document);
-
-//   @override
-//   _PatientReportState createState() => _PatientReportState();
-// }
-
-// class _PatientReportState extends State<PatientReport> {
-//   TextEditingController _medicationsController = TextEditingController();
-
-//   String _medications = '';
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Patient Report'),
-//         leading: GestureDetector(
-//           onTap: () {
-//             Navigator.pushNamed(context, 'doctor');
-//           },
-//           child: const Icon(
-//             Icons.arrow_back_ios,
-//             size: 20,
-//             color: Colors.white,
-//           ),
-//         ),
-//       ),
-//       body: StreamBuilder<DocumentSnapshot>(
-//         stream: FirebaseFirestore.instance
-//             .collection('patientsinfo')
-//             .doc(widget.document.id)
-//             .snapshots(),
-//         builder:
-//             (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//           if (!snapshot.hasData) {
-//             return Center(child: CircularProgressIndicator());
-//           }
-//           Map<String, dynamic> patientData =
-//               snapshot.data!.data() as Map<String, dynamic>;
-//           String patientName = patientData['name'] ?? 'Unknown';
-//           String age = patientData['Age'] ?? 'Unknown';
-//           String diagnosis = patientData['diagnosis'] ?? 'Unknown';
-//           return ListView(
-//             padding: EdgeInsets.all(16.0),
-//             children: <Widget>[
-//               Container(
-//                 padding: EdgeInsets.all(16.0),
-//                 decoration: BoxDecoration(
-//                   border: Border.all(
-//                     color: Colors.blue,
-//                     width: 2.0,
-//                   ),
-//                   borderRadius: BorderRadius.circular(10.0),
-//                 ),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: <Widget>[
-//                     Text(
-//                       'Patient Name:',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                     SizedBox(height: 8.0),
-//                     Text(
-//                       patientName,
-//                       style: TextStyle(fontSize: 16.0),
-//                     ),
-//                     SizedBox(height: 16.0),
-//                     Text(
-//                       'Patient Age:',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                     SizedBox(height: 8.0),
-//                     Text(
-//                       age,
-//                       style: TextStyle(fontSize: 16.0),
-//                     ),
-//                     SizedBox(height: 16.0),
-//                     Text(
-//                       'Diagnosis:',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                     SizedBox(height: 8.0),
-//                     Text(
-//                       diagnosis,
-//                       style: TextStyle(fontSize: 16.0),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               SizedBox(height: 16.0),
-//               Container(
-//                 padding: EdgeInsets.all(16.0),
-//                 decoration: BoxDecoration(
-//                   border: Border.all(
-//                     color: Colors.blue,
-//                     width: 2.0,
-//                   ),
-//                   borderRadius: BorderRadius.circular(10.0),
-//                 ),
-//                 child: Container(
-//                   padding: EdgeInsets.all(16.0),
-//                   decoration: BoxDecoration(
-//                     border: Border.all(
-//                       color: Colors.blue,
-//                       width: 2.0,
-//                     ),
-//                     borderRadius: BorderRadius.circular(10.0),
-//                   ),
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: <Widget>[
-//                       Text(
-//                         'Medications:',
-//                         style: TextStyle(
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                       SizedBox(height: 8.0),
-//                       TextField(
-//                         controller: _medicationsController,
-//                         onChanged: (value) {
-//                           setState(() {
-//                             _medications = value;
-//                           });
-//                         },
-//                         decoration: InputDecoration(
-//                           hintText: 'Enter medications',
-//                           border: OutlineInputBorder(),
-//                         ),
-//                       ),
-//                       SizedBox(height: 16.0),
-//                       ElevatedButton(
-//                         onPressed: () async {
-//                           await FirebaseFirestore.instance
-//                               .collection('patientsinfo')
-//                               .doc(widget.document.id)
-//                               .update({'medications': _medications});
-//                           Navigator.pop(context);
-//                         },
-//                         child: Text('Save'),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
