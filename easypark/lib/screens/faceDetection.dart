c4c3fe729517847d0f66610a4a0b771bac0761d0
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-05-12 14:30:14 +0300

    update the face detection

--- easypark/lib/screens/faceDetection.dart
+++ easypark/lib/screens/faceDetection.dart
@@ -1,139 +1,165 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:photo_view/photo_view.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'dart:io';
+// import 'package:photo_view/photo_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:path/path.dart';
+// import 'package:http/http.dart' as http;
 
-class faceDetection extends StatefulWidget {
-  const faceDetection({super.key});
+// class faceDetection extends StatefulWidget {
+//   const faceDetection({super.key});
 
-  @override
-  State<faceDetection> createState() => _faceDetectionState();
-}
+//   @override
+//   State<faceDetection> createState() => _faceDetectionState();
+// }
 
-class _faceDetectionState extends State<faceDetection> {
-  File? selectedImage;
-  String? message = "";
+// class _faceDetectionState extends State<faceDetection> {
+//   File? selectedImage;
+//   String? message = "";
 
-  uploadImage() async {
-    var url = "http://192.168.1.3:8000/upload";
-    final request = http.MultipartRequest("POST", Uri.parse(url));
+//   void uploadImage() async {
+//     // Create a list of individual file objects
+//     List<File> files =
+//         _images.where((image) => image != null).map((image) => image!).toList();
 
-    final headers = {"Content-type": "multipart/form-data"};
+//     // Send the files to the API using a multipart/form-data request
+//     var request = http.MultipartRequest(
+//         'POST', Uri.parse('http://192.168.1.3:8000/upload'));
+//     files.forEach((file) {
+//       request.files.add(http.MultipartFile(
+//           'images[]', file.readAsBytes().asStream(), file.lengthSync(),
+//           filename: basename(file.path)));
+//     });
 
-    request.files.add(http.MultipartFile('image',
-        selectedImage!.readAsBytes().asStream(), selectedImage!.lengthSync(),
-        filename: selectedImage!.path.split("/").last));
+//     // Send the request
+//     var response = await request.send();
 
-    request.headers.addAll(headers);
-    final response = await request.send();
-    http.Response res = await http.Response.fromStream(response);
-    if (response.statusCode == 200) {
-      final resJson = jsonDecode(res.body);
-      message = resJson['message'].toString();
-      setState(() {});
-      print('here');
-    } else {
-      print('Failed ${response.statusCode}');
-      message = 'failed';
-    }
-  }
+//     // Handle the response
+//     if (response.statusCode == 200) {
+//       // Request successful
+//     } else {
+//       // Request failed
+//     }
+//   }
 
-  Future getImage() async {
-    final pickedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    selectedImage = File(pickedImage!.path);
-    setState(() {});
-  }
+//   // Future getImage() async {
+//   //   final pickedImage =
+//   //       await ImagePicker().pickImage(source: ImageSource.gallery);
+//   //   selectedImage = File(pickedImage!.path);
+//   //   setState(() {});
+//   // }
+//   Future<void> getImage(int index) async {
+//     final pickedFile =
+//         await ImagePicker().getImage(source: ImageSource.gallery);
+//     setState(() {
+//       if (pickedFile != null) {
+//         _images[index] = File(pickedFile.path);
+//       }
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, 'home');
-          },
-          child: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.white,
-          ),
-        ),
-        title: const Text("Upload The Face Images"),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildImagePicker(
-                'Pick a smiling image of you',
-                Icons.tag_faces,
-                () => getImage(),
-              ),
-              const SizedBox(height: 16.0),
-              _buildImagePicker(
-                'Pick a disgusted face image of you',
-                Icons.sentiment_very_dissatisfied,
-                () => getImage(),
-              ),
-              const SizedBox(height: 16.0),
-              _buildImagePicker(
-                'Pick a surprised face image of you',
-                Icons.sentiment_very_satisfied,
-                () => getImage(),
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: const Icon(Icons.add_a_photo),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: GestureDetector(
+//           onTap: () {
+//             Navigator.pushNamed(context, 'home');
+//           },
+//           child: const Icon(
+//             Icons.arrow_back_ios,
+//             size: 20,
+//             color: Colors.white,
+//           ),
+//         ),
+//         title: const Text("Upload The Face Images"),
+//       ),
+//       body: Center(
+//         child: Container(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               _buildImagePicker(
+//                 'Pick a smiling image of you',
+//                 Icons.tag_faces,
+//                 () => uploadImage(),
+//               ),
+//               const SizedBox(height: 16.0),
+//               _buildImagePicker(
+//                 'Pick a disgusted face image of you',
+//                 Icons.sentiment_very_dissatisfied,
+//                 () => uploadImage(),
+//               ),
+//               const SizedBox(height: 16.0),
+//               _buildImagePicker(
+//                 'Pick a surprised face image of you',
+//                 Icons.sentiment_very_satisfied,
+//                 () => uploadImage(),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: getImage(3),
+//         child: const Icon(Icons.add_a_photo),
+//       ),
+//     );
+//   }
 
-  Widget _buildImagePicker(
-      String title, IconData icon, VoidCallback onPressed) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue[600]),
-              ),
-              onPressed: onPressed,
-              icon: Icon(
-                icon,
-                color: Colors.white,
-              ),
-              label: const Text(
-                'Upload',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 8.0),
-            Text('. $message'),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   List<File?> _images = List.generate(3, (_) => null);
+
+//   Widget _buildImagePicker(String title, IconData icon, int index) {
+//     return Column(
+//       children: <Widget>[
+//         Text(title),
+//         IconButton(
+//           icon: Icon(icon),
+//           onPressed: () => getImage(index),
+//         ),
+//         _images[index] != null ? Image.file(_images[index]!) : Placeholder(),
+//       ],
+//     );
+//   }
+
+//   // Widget _buildImagePicker(
+//   //     String title, IconData icon, VoidCallback onPressed) {
+//   //   return Card(
+//   //     child: Padding(
+//   //       padding: const EdgeInsets.all(16.0),
+//   //       child: Column(
+//   //         crossAxisAlignment: CrossAxisAlignment.stretch,
+//   //         children: [
+//   //           Text(
+//   //             title,
+//   //             style: const TextStyle(
+//   //               fontSize: 18.0,
+//   //               fontWeight: FontWeight.bold,
+//   //             ),
+//   //           ),
+//   //           const SizedBox(height: 16.0),
+//   //           ElevatedButton.icon(
+//   //             style: ButtonStyle(
+//   //               backgroundColor: MaterialStateProperty.all(Colors.blue[600]),
+//   //             ),
+//   //             onPressed: onPressed,
+//   //             icon: Icon(
+//   //               icon,
+//   //               color: Colors.white,
+//   //             ),
+//   //             label: const Text(
+//   //               'Upload',
+//   //               style: TextStyle(color: Colors.white),
+//   //             ),
+//   //           ),
+//   //           const SizedBox(height: 8.0),
+//   //           Text('. $message'),
+//   //         ],
+//   //       ),
+//   //     ),
+//   //   );
+//   // }
+// }
