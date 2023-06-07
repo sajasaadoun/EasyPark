e45ea44c65e08010b4a6624d5b4991cd6d786e30
sajasaadoun <saja1904826@miuegypt.edu.eg>
2023-06-07 18:00:46 +0300

    main

--- easypark/lib/main.dart
+++ easypark/lib/main.dart
@@ -77,7 +77,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: 'face',
+        initialRoute: 'login',
         routes: {
           'voiceUI': (context) => const AudioPage(),
           '/ar/home': (context) => const HomePageeAr(),
