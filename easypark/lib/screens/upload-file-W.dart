89951b4c666ea8569580f7bf7eb841c76ed2ffb6
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-06-21 15:54:53 +0300

    update upload file w

--- easypark/lib/screens/upload-file-W.dart
+++ easypark/lib/screens/upload-file-W.dart
@@ -43,12 +43,12 @@ class _MainPageState extends State<MainPage> {
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
+        // automaticallyImplyLeading: true,
         title: Text(uploadFileW.title),
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
+          onPressed: () => Navigator.pushNamed(context, '/home'),
         ),
       ),
       body: Container(
