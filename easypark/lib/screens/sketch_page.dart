0e30e35de89599a9cbaa04b3268a430f363661e0
nathaliesameh26 <100384701+nathaliesameh26@users.noreply.github.com>
2023-06-21 13:52:06 +0300

    remove dart:ffi

--- easypark/lib/screens/sketch_page.dart
+++ easypark/lib/screens/sketch_page.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:gallery_saver/files.dart';
